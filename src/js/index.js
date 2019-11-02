f785e8a852328048889c7fde907f7e60f60240d8
Román Pozzo <rhpozzo@gmail.com>
2019-11-02 15:48:58 +0000

    Added functionalty

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,16 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pinta = ["\u2660", "\u2663", "\u2665", "\u2665"];
+  let number = Math.floor(Math.random() * 10 + 2);
+  let royalty = [number, "A", "K", "Q", "J", number, number, number];
+
+  // let result = (pinta[Math.floor(Math.random() * pinta.length)] +=
+  //   royalty[Math.floor(Math.random() * royalty.length)]);
+
+  document.querySelector(".pinta").innerHTML =
+    pinta[Math.floor(Math.random() * pinta.length)];
+
+  document.querySelector(".number").innerHTML =
+    royalty[Math.floor(Math.random() * royalty.length)];
 };
